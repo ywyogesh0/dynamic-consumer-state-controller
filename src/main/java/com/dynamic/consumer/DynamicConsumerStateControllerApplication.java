551384640a8aadc99ea9960e89228b85ec0225cf
ywyogesh0 <ywyogesh0@gmail.com>
2024-07-01 11:15:14 +0100

    second commit

---
+++ src/main/java/com/dynamic/consumer/DynamicConsumerStateControllerApplication.java
@@ -0,0 +1,13 @@
+package com.dynamic.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicConsumerStateControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicConsumerStateControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dynamic/consumer/DynamicConsumerStateControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dynamic.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicConsumerStateControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
